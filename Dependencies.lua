7ac3221039ac5021d415eb7b27165a29d0d0c897
Bruno Moreira <brunomcmoreira99@gmail.com>
2025-05-10 07:26:49 +0100

    Remove invalid stuff from Dependencies.lua

--- Dependencies.lua
+++ Dependencies.lua
@@ -1,5 +1,3 @@
 IncludeDir = {}
-IncludeDir["GLEW"] = "%{wks.location}/Roose/vendor/GLEW/include"
-IncludeDir["GLFW"] = "%{wks.location}/Roose/vendor/GLFW/include"
 IncludeDir["glm"] = "%{wks.location}/Roose/vendor/glm"
 IncludeDir["stb_image"] = "%{wks.location}/Roose/vendor/stb_image"
