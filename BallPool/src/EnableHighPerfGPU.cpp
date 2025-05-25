fec51e684e386d95c194af2c4289efd105ec303a
Bruno Moreira <brunomcmoreira99@gmail.com>
2025-05-25 01:45:36 +0100

    Fix
    - Prevent EnableHighPerfGPU.cpp from running on incompatible OS or compilers

--- BallPool/src/EnableHighPerfGPU.cpp
+++ BallPool/src/EnableHighPerfGPU.cpp
@@ -1,6 +1,15 @@
-// Enforces the use of the high-performance GPU on systems with hybrid graphics (NVIDIA Optimus or AMD PowerXpress).
+#include <Roose/Core/PlatformDetection.h>
 
-extern "C" {
-    _declspec(dllexport) unsigned int NvOptimusEnablement = 0x00000001;
-    _declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
-}
+#ifdef RS_PLATFORM_WINDOWS
+    #ifdef _MSC_VER
+        #define EXPORT_SYMBOL __declspec(dllexport)
+    #else
+        #define EXPORT_SYMBOL __attribute__((visibility("default")))
+    #endif
+
+    // Enforces the use of the high-performance GPU on systems with hybrid graphics (NVIDIA Optimus or AMD PowerXpress).
+    extern "C" {
+        EXPORT_SYMBOL unsigned int NvOptimusEnablement = 0x00000001;
+        EXPORT_SYMBOL int AmdPowerXpressRequestHighPerformance = 1;
+    }
+#endif
